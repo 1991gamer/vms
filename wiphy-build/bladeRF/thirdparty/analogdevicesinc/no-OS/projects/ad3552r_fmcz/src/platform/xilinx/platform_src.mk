0f8bcf4ead4be8c1160a6c256a7f7fe264938245
1991gamer <1991gamer>
2025-01-01 19:51:07 -0500

    bad ref

--- wiphy-build/bladeRF/thirdparty/analogdevicesinc/no-OS/projects/ad3552r_fmcz/src/platform/xilinx/platform_src.mk
+++
@@ -1,5 +0,0 @@
-SRCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.c
-SRCS += $(PLATFORM_DRIVERS)/$(PLATFORM)_delay.c
-
-INCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.h \
-	$(INCLUDE)/no_os_gpio.h
